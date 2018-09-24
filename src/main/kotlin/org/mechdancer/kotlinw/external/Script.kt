b80ea58538a8c759e15c102fdce97b555e7d25f6
YangDR <ydrml@hotmail.com>
2018-09-24 08:17:39 +0800

    添加脚本参数和脚本返回值类型的泛型指定
    Signed-off-by: YangDR <ydrml@hotmail.com>

--- src/main/kotlin/org/mechdancer/kotlinw/external/Script.kt
+++ src/main/kotlin/org/mechdancer/kotlinw/external/Script.kt
@@ -7,28 +7,35 @@ import org.mechdancer.kotlinw.core.EnvironmentUtil
 /**
  * 将一段代码文本视作脚本并转换为函数
  */
-fun script(
+fun <R> Any?.script(
 	action: String,
 	`package`: String = "script",
-	reference: List<String> = listOf()
+	vararg reference: String
 ) =
-	StringBuilder().apply {
-		if (`package`.isNotBlank()) append("package $`package`\n")
-		reference.forEach { append("import $it\n") }
-		append("""
-			fun f() = run {
+	StringBuilder()
+		.apply {
+			if (`package`.isNotBlank()) append("package $`package`\n")
+			reference.forEach { append("import $it\n") }
+			"""
+			fun Any?.f() = run {
 				$action
 			}
-		""".trimIndent())
-	}
+			""".trimIndent().let(::append)
+		}
 		.toString()
-		.let {
+		.run {
 			val environment = EnvironmentUtil.createEnvironment()
-			val ktFile = Analyzer.parse(it, environment)
+			val ktFile = Analyzer.parse(this@run, environment)
 			val result = Compiler.compile(ktFile, environment)
+
 			Compiler
 				.loadClasses(result)[0]
-				.getMethod("f")
-		}.let { { it(null) } }
-
-
+				.methods
+				.first { it.name == "f" }
+				.let { function ->
+					{
+						@Suppress("UNCHECKED_CAST")
+						function.invoke(null, this@script) as R
+					}
+				}
+		}
--- src/test/kotlin/TestScript.kt
+++ src/test/kotlin/TestScript.kt
@@ -1,11 +1,18 @@
+import org.junit.Assert
+import org.junit.Test
 import org.mechdancer.kotlinw.external.script
 
-fun main(args: Array<String>) {
-	"""
-		println("hello world")
-		"ok"
-	"""
-		.trimIndent()
-		.run { script(this)() }
-		.let(::println)
+class TestScript {
+	@Test
+	fun test() {
+		val r =
+			"""
+				val num = this as Int
+				println("hello world")
+				num * 2
+			"""
+				.trimIndent()
+				.run { 100.script<Int>(this)() }
+		Assert.assertEquals(200, r)
+	}
 }
